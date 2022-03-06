0297414d6ae457c4a40f71dc1ca17997a397ff4c
Haja Rasoahaingo <haja.rasoah@gmail.com>
2022-03-06 18:22:58 +0100

    Try fix deploy: add babel plugins for graphql imports

--- next.config.js
+++ next.config.js
@@ -6,8 +6,10 @@ module.exports = {
   },
   reactStrictMode: true,
   webpack: (config, options) => {
+    console.log(options);
     config.module.rules.push({
       test: /\.(graphql|gql)$/,
+      include: [options.dir],
       exclude: /node_modules/,
       use: [options.defaultLoaders.babel, { loader: 'graphql-tag/loader' }],
     });
